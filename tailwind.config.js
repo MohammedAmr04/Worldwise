1738b2fd1cc810ddaf451b423bbb0b6ee37981b5
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-04-14 04:37:54 +0200

    Create init files

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        codystar: ['"Codystar"', "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
